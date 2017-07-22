307a590985d79b8675475761acc07e51ce5a9641
Marcin Zyla <marcin.zyla0@gmail.com>
2017-07-22 12:52:07 +0200

    [F] Add controller interface stub

---
+++ controller_iface.hpp
@@ -0,0 +1,22 @@
+/**
+* @file controller_iface.hpp
+* @author Marcin Zyla
+* @date 22.07.2017
+*
+* This file contains MVC Controller interface
+*/
+
+#ifndef _CONTROLLER_IFACE_H 
+#define _CONTROLLER_IFACE_H
+
+namespace Gravsim {
+
+	class ControllerIface {
+		///Empty destructor
+		virtual ~ControllerIface() {};
+	};
+
+}
+
+
+#endif _CONTROLLER_IFACE_H
