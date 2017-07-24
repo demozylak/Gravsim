cb7df706f3c5daf6cab605471071445bb6bf1c5f
Marcin Zyla <marcin.zyla0@gmail.com>
2017-07-24 20:14:47 +0200

    [F] Add view class stubs

---
+++ view.cpp
@@ -0,0 +1,10 @@
+/**
+* @file view.cpp
+* @author Marcin Zyla
+* @date 23.07.2017
+*
+* This file contains MVC View definition
+*/
+
+#include "view.hpp"
+
---
+++ view.hpp
@@ -0,0 +1,17 @@
+/**
+* @file view.hpp
+* @author Marcin Zyla
+* @date 23.07.2017
+*
+* This file contains MVC View declaration
+*/
+
+#pragma once
+namespace Gravsim {
+
+	class View {
+	public:
+
+	};
+
+}
