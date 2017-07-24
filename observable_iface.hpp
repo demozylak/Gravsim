9cfd0db4ba549b5cdaff4fa2d27167da8e768341
Marcin Zyla <marcin.zyla0@gmail.com>
2017-07-25 01:03:05 +0200

    [F] Add observer and observable interfaces

---
+++ observable_iface.hpp
@@ -0,0 +1,18 @@
+/**
+* @file observable_iface.hpp
+* @author Marcin Zyla
+* @date 25.07.2017
+*
+* This file contains observable interface used in observer pattern
+*/
+#pragma once
+#include "iface_macro.hpp"
+#include "observer_iface.hpp"
+
+namespace Gravsim {
+	template<T>
+	INTERFACE_BEGIN(Observable)
+		virtual void registerObserver(ObserverIface<T>) = 0;
+		virtual void unregisterObserver(ObserverIface<T>) = 0;
+	INTERFACE_END()
+}
---
+++ observer_iface.hpp
@@ -0,0 +1,17 @@
+/**
+* @file observer_iface.hpp
+* @author Marcin Zyla
+* @date 25.07.2017
+*
+* This file contains observer interface used in observer pattern
+*/
+#pragma once
+#include "iface_macro.hpp"
+
+namespace Gravsim {
+
+	template<T>
+	INTERFACE_BEGIN(Observer)
+		virtual void notifyObserver(T) = 0;
+	INTERFACE_END()
+}
