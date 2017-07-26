7f9c73b5cbdb3dd2db1a5653b563dd291182d79e
Marcin Zyla <marcin.zyla0@gmail.com>
2017-07-26 20:15:32 +0200

    [I] Add simulation model stub

---
+++ Model/simulation_model.cpp
@@ -0,0 +1,12 @@
+/**
+* @file simulation_model.cpp
+* @author Marcin Zyla
+* @date 26.07.2017
+*
+* This file contains model for the application
+*/
+
+#include "simulation_model.hpp"
+
+using namespace Gravsim;
+
---
+++ Model/simulation_model.hpp
@@ -0,0 +1,19 @@
+/**
+* @file simulation_model.hpp
+* @author Marcin Zyla
+* @date 25.07.2017
+*
+* This file contains application model declaration
+*/
+#pragma once
+
+#include "../MVCIface/simulation_model_iface.hpp"
+
+namespace Gravsim {
+
+	class Simulation : public SimulationModelIface{
+	public:
+
+	};
+
+}
