abaa4ddeafe9c8dde17890efd8fc945f1efbed48
Marcin Zyla <marcin.zyla0@gmail.com>
2017-07-29 15:53:02 +0200

    [I] Removed function name from logs

--- util/log.hpp
+++ util/log.hpp
@@ -12,7 +12,7 @@
 #ifdef _DEBUG
 	#include <iostream>
 	#define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
-	#define LOG(severity, msg) std::cout << "[" << __FUNCTION__ << " @ " << __FILENAME__ << ":" << __LINE__ << "][" << severity << "] " << msg << std::endl;
+	#define LOG(severity, msg) std::cout << "["  << __FILENAME__ << ":" << __LINE__ << "][" << severity << "] " << msg << std::endl;
 	#define INFO "INFO"
 	#define DEB "DEBUG"
 	#define WARN "WARNING"
